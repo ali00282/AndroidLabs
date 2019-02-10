21fdeaf1317df62d17edfa85a45d22b076a4caca
Tariq Ali <ali00282@algonquinlive.com>
2019-02-10 16:26:06 -0500

    Lab3 Completed

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.androidlab.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailEditText;
+    SharedPreferences sp;
+    String typedEmail;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.lab3layout);
+
+        emailEditText = (EditText)findViewById(R.id.EmailEditText);
+        sp = getSharedPreferences("name", Context.MODE_PRIVATE);
+        login = (Button)findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this, ProfitActivity.class);
+
+                //EditText et = (EditText)findViewById(R.id.EmailEditText);
+
+                it.putExtra("emailTyped", emailEditText.getText().toString());
+
+                startActivityForResult(it, 345);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // This is an editor object
+        SharedPreferences.Editor edit = sp.edit();
+
+
+        // this was we are saving what user has type.
+        // This is similar to Java String = next().
+        typedEmail = emailEditText.getText().toString();
+
+        // This edit.putString() method saves email that is typed by user
+        edit.putString("",typedEmail);
+
+        // It apply changes that are made
+        edit.apply();
+
+
     }
 }
